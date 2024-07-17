afc08a857c39b66994aa5571faa954da62b2fe35
joaoD <joaoldp135@gmail.com>
2024-07-17 20:37:58 -0300

    ajustes info

--- src/pages/info/Informacoes.jsx
+++ src/pages/info/Informacoes.jsx
@@ -22,15 +22,15 @@ function Informacoes() {
             <img src={assets.images.logo_full_light} style={{ height: 'auto', width: '475px' }} alt={"Logo do Syphon.io"}/>
             <p style={paragraph}>
                 Syphon.io é um projeto com o objetivo de experimentar com a implementação da plataforma de armazenamento 
-                distribuído <a style={aStyle} href="https://ipfs.tech">IPFS</a> como alternativa para armazenamento em 
+                distribuído <a style={aStyle} href="https://ipfs.tech" target="_blank">IPFS</a> como alternativa para armazenamento em 
                 nuvem pessoal, buscando trazer conveniência e simplicidade e eliminar o controle desregulado sobre os 
                 dados pessoais dos usuários que plataformas centralizadas podem ter.
             </p>
             <p style={paragraph}>
-                Para começar, na seção serviços você pode cadastrar <a style={aStyle} href="/servicos">serviços</a> de pinning, a funcionalidade de persistência de dados do 
-                IPFS, podendo escolher entre os provedores <a style={aStyle} href="https://www.pinata.cloud">Piñata</a> e <a style={aStyle} href="https://filebase.com">Filebase</a>, 
-                e em seguida, na seção <a style={aStyle} href="/">diretórios</a>, você pode montar uma estrutura de diretórios e subdiretórios, assim como realizar o upload de arquivos, que serão 
-                criptografados e descriptografados utilizando uma chave privada que apenas você pode ver na seção de <a style={aStyle} href="/config">configurações</a>, onde você também poderá 
+                Para começar, na seção serviços você pode cadastrar <a style={aStyle} href="/servicos" target="_blank">serviços</a> de pinning, a funcionalidade de persistência de dados do 
+                IPFS, podendo escolher entre os provedores <a style={aStyle} href="https://www.pinata.cloud" target="_blank">Piñata</a> e <a style={aStyle} href="https://filebase.com" target="_blank">Filebase</a>, 
+                e em seguida, na seção <a style={aStyle} href="/" target="_blank">diretórios</a>, você pode montar uma estrutura de diretórios e subdiretórios, assim como realizar o upload de arquivos, que serão 
+                criptografados e descriptografados utilizando uma chave privada que apenas você pode ver na seção de <a style={aStyle} href="/config" target="_blank">configurações</a>, onde você também poderá 
                 alterar alguns dados de sua conta.
             </p>
         </div>
